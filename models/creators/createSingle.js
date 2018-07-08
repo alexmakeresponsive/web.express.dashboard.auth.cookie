ee0b9dd8f30dbd108ee6633b2d3750282e47a45a
Alexandr Gorchakov <alexmakeresponsive@mail.ru>
2018-07-08 20:16:21 +0600

    add crateDB script

--- models/creators/createSingle.js
+++ models/creators/createSingle.js
@@ -1,16 +1,19 @@
-var User = require('../users').User;
+var mongoose = require('../../libs/mongoose');
+var User     = require('../users').User;
 
 var user = new User({
-    username: "Tester5",
+    username: "Tester8",
     password: "secret"
 });
 
 user.save(function(err, user, affected) {
     if (err) throw err;
 
-    User.findOne({username: "Tester5"}, function(err, tester) {
+    User.findOne({username: "Tester8"}, function(err, tester) {
         if (err) throw err;
 
         console.log(tester);
+
+        mongoose.disconnect();
     });
 });
